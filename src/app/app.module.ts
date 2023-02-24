25be7d8aa9de41b6ab3c499a278cdbab79b8444c
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-02-24 17:06:10 -0300

    update dependences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -36,7 +36,6 @@ import { EditarComponent } from './components/editar/editar.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
