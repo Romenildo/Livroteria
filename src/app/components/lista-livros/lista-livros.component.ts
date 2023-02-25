43e4e5e9360fc6f2e61de6a6054af261e619e634
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-02-25 16:19:53 -0300

    adicionado animacoes do angular

--- src/app/components/lista-livros/lista-livros.component.ts
+++ src/app/components/lista-livros/lista-livros.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Livro } from 'src/app/model/livro.model';
-import { Autor } from 'src/app/model/autor.model';
 import { LivroService } from 'src/app/services/livro.service';
 import { ApiService } from 'src/app/services/api.service';
+import { style, animate,transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.scss']
+  styleUrls: ['./lista-livros.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        style({ opacity: 0, transform: "translateX(-100%)" }),
+        animate(
+          "400ms ease-in-out",
+          style({ opacity: 1, transform: "translateX(0)" })
+        )
+      ]),   
+    ])
+  ]
 })
+
 export class ListaLivrosComponent implements OnInit {
   livros:Livro[] = []
   @Input() modoLista?: string;
